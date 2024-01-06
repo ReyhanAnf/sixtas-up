3bf809714981d0b408a980221b4aec173f8f9f78
Reyhan Andrea Firdaus <reihan676590@gmail.com>
2024-01-06 00:36:34 +0000

    revoke frontend

--- frontend/app/ui/fonts.ts
+++
@@ -1,8 +0,0 @@
-import { Poppins, Inter } from 'next/font/google';
- 
-export const inter = Inter({ subsets: ['latin'] });
-
-export const poppins = Poppins({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-});
